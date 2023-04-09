eff4906f34b6db17a53fe55a2b7f2749326f1636
iosebkhe <ioseb.khetsuriani@yahoo.com>
2023-04-09 21:47:06 +0400

    final fixes

--- script.js
+++ script.js
@@ -1,31 +1,119 @@
-// const sliderWrapper = document.querySelector(".slider__wrapper");
-// const sliderButtonPrev = document.querySelector(".slider__button-prev");
-// const sliderButtonNext = document.querySelector(".slider__button-next");
-// const slideWidth = sliderWrapper.clientWidth / 3; // adjust for number of visible slides
-// let currentSlide = 0;
+const sliderLogoContainer = document.querySelector(".slider__wrapper");
+const slidesLogos = document.querySelectorAll(".slide-logo");
+const btnNextLogo = document.querySelector(".btn-slider-logo--next");
+const btnPrevLogo = document.querySelector(".btn-slider-logo--prev");
+const sliderTestimonialContainer = document.querySelector(
+  ".testimonials__wrapper"
+);
+const slideTestimonials = Array.from(
+  document.querySelectorAll(".testimonial-img")
+);
+const btnNextTestimonial = document.querySelector(
+  ".btn-slider-testimonial--next"
+);
+const btnPrevTestimonial = document.querySelector(
+  ".btn-slider-testimonial--prev"
+);
 
-// function moveSlider() {
-//   sliderWrapper.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
-// }
+// navigation buttons
+const headerMobNavBtn = document.querySelector(".nav-header__icon");
+const footerMobNavBtn = document.querySelector(".footer__nav-icon");
 
-// function nextSlide() {
-//   currentSlide++;
-//   if (currentSlide >= sliderWrapper.children.length - 2) {
-//     // adjust for number of visible slides
-//     currentSlide = 0;
-//   }
-//   moveSlider();
-// }
+// logo slider
+const slider = function () {
+  let currentSlide = 0;
+  const maxSlide = slidesLogos.length;
 
-// function prevSlide() {
-//   currentSlide--;
-//   if (currentSlide < 0) {
-//     currentSlide = sliderWrapper.children.length - 3; // adjust for number of visible slides
-//   }
-//   moveSlider();
-// }
+  /////////////functions
 
-// sliderButtonNext.addEventListener("click", nextSlide);
-// sliderButtonPrev.addEventListener("click", prevSlide);
+  //default slide
+  const goToSlide = function (slide) {
+    slidesLogos.forEach(function (s, index) {
+      s.style.transform = `translateX(${50 * (index - slide)}%)`;
+    });
+  };
 
-// setInterval(nextSlide, 2000);
+  //going next slide
+  const nextSlide = function () {
+    if (currentSlide === maxSlide - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+
+    goToSlide(currentSlide);
+  };
+
+  //going prev slide
+  const prevSlide = function () {
+    if (currentSlide === 0) {
+      currentSlide = maxSlide - 1;
+    } else {
+      currentSlide--;
+    }
+
+    goToSlide(currentSlide);
+  };
+
+  goToSlide(0);
+
+  //Event handlers
+  btnNextLogo.addEventListener("click", nextSlide);
+  btnPrevLogo.addEventListener("click", prevSlide);
+};
+slider();
+
+// testimonial slider
+const sliderTestimonial = function () {
+  let currentSlide = 0;
+  const maxSlide = slideTestimonials.length;
+
+  /////////////functions
+
+  //default slide
+  const goToSlide = function (slide) {
+    slideTestimonials.forEach(function (s, index) {
+      s.style.transform = `translateX(${50 * (index - slide)}%)`;
+    });
+  };
+
+  //going next slide
+  const nextSlide = function () {
+    if (currentSlide === maxSlide - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+
+    goToSlide(currentSlide);
+  };
+
+  //going prev slide
+  const prevSlide = function () {
+    if (currentSlide === 0) {
+      currentSlide = maxSlide - 1;
+    } else {
+      currentSlide--;
+    }
+
+    goToSlide(currentSlide);
+  };
+
+  goToSlide(0);
+
+  //Event handlers
+  btnNextTestimonial.addEventListener("click", nextSlide);
+  btnPrevTestimonial.addEventListener("click", prevSlide);
+};
+sliderTestimonial();
+
+headerMobNavBtn.addEventListener("click", function () {
+  document
+    .querySelector(".nav-header__list")
+    .classList.toggle("active-nav-header");
+});
+footerMobNavBtn.addEventListener("click", function () {
+  document
+    .querySelector(".footer__nav-list")
+    .classList.toggle("active-nav-footer");
+});
